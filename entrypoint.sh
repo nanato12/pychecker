d8659d4453b00525e65c62f63ba5bfe956865e12
nanato12 <admin@nanato12.info>
2020-11-19 00:51:25 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,14 @@
 
 set -ea
 
+echo -e "\nstart date: $(date)\n"
+
 echo "##################################################"
 echo "#                                                #"
 echo "#                Starting pycheck                #"
 echo "#                                                #"
 echo "##################################################"
-echo ""
+echo -e "\n"
 
 echo -e "Path to check: $@\n"
 
@@ -18,14 +20,21 @@ echo -e "\n$(python --version)"
 echo -e "$(pip --version)\n"
 
 # black
+TIME_A=`date +%s`
 echo -e "$(black --version)"
 black --check "$@"
+TIME_B=`date +%s`
+BLACK_PROCCESS_TIME=$((TIME_B-TIME_A))
 
 # flake8
+TIME_A=`date +%s`
 echo -e "\nflake8 $(flake8 --version)"
 flake8 "$@"
+TIME_B=`date +%s`
+FLAKE8_PROCCESS_TIME=$((TIME_B-TIME_A))
 
 # mypy
+TIME_A=`date +%s`
 echo -e "\n$(mypy --version)"
 for file_name in `find $@`; do
     if [ "py" == `basename $file_name | sed 's/^.*\.\([^\.]*\)$/\1/'` ];
@@ -33,10 +42,15 @@ for file_name in `find $@`; do
         mypy --check $file_name
     fi
 done
+TIME_B=`date +%s`
+MYPY_PROCCESS_TIME=$((TIME_B-TIME_A))
 
 # isort
+TIME_A=`date +%s`
 echo "$(isort --version)"
 isort --check "$@"
+TIME_B=`date +%s`
+ISORT_PROCCESS_TIME=$((TIME_B-TIME_A))
 
 echo ""
 echo "##################################################"
@@ -45,4 +59,11 @@ echo "#                Complete pycheck                #"
 echo "#                                                #"
 echo "##################################################"
 
-echo -e "\nfinish date: $(date)"
+echo -e "\n\nProcess time\n"
+
+echo "black: ${BLACK_PROCCESS_TIME}s"
+echo "flake8: ${FLAKE8_PROCCESS_TIME}s"
+echo "mypy: ${MYPY_PROCCESS_TIME}s"
+echo "isort: ${ISORT_PROCCESS_TIME}s"
+
+echo -e "\nfinish date: $(date)\n"
